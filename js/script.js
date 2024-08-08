555cc2f307cba219a3b6cea34fc11586f9dbc383
Tatsiana <badpanda@tut.by>
2024-08-08 20:37:42 +0200

    Slider with style.transform is implemented instead an easy slider.

--- js/script.js
+++ js/script.js
@@ -301,12 +301,78 @@ window.addEventListener('DOMContentLoaded', function() {
 
 // slider
 
-const slides = this.document.querySelectorAll('.offer__slide'),
-    prev = this.document.querySelector('.offer__slider-prev'),
-    next = this.document.querySelector('.offer__slider-next');
+const slides = document.querySelectorAll('.offer__slide'),
+    prev = document.querySelector('.offer__slider-prev'),
+    total = document.querySelector('#total'),
+    next = document.querySelector('.offer__slider-next'),
+    current = document.querySelector('#current');
+    slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+    slidesField = document.querySelector('.offer__slider-inner'),
+    width = window.getComputedStyle(slidesWrapper).width;
 let slideIndex = 1;
+let offset = 0;
 
-showSlides(slideIndex);
+if(slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slideIndex}`;
+} else {
+    total.textContent = slides.length;
+    current.textContent = slideIndex;
+}
+
+slidesField.style.width = 100 * slides.length + '%';
+slidesField.style.display = 'flex';
+slidesField.style.transition = '0.5s all';
+
+slidesWrapper.style.overflow = 'hidden';
+
+slides.forEach(slide => {
+    slide.style.width = width;
+})
+
+next.addEventListener('click', () => {
+    if(offset == +width.slice(0,width.length-2) * (slides.length - 1)){
+        offset = 0;
+    } else {
+        offset += +width.slice(0,width.length-2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px`;
+
+    if(slideIndex == slides.length) {
+        slideIndex = 1;
+    } else {
+        slideIndex++;
+    }
+
+    if(slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+    } else {
+        current.textContent = slideIndex;
+    }
+});
+
+prev.addEventListener('click', () => {
+    if(offset == 0){
+        offset = +width.slice(0,width.length-2) * (slides.length - 1);
+    } else {
+        offset -= +width.slice(0,width.length-2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px`;
+
+    if(slideIndex == 1) {
+        slideIndex = slides.length;
+    } else {
+        slideIndex--;
+    }
+
+    if(slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+    } else {
+        current.textContent = slideIndex;
+    }
+});
+
+/*showSlides(slideIndex);
 
     function showSlides(n){
         if(n > slides.length) {
@@ -333,5 +399,5 @@ showSlides(slideIndex);
 
     next.addEventListener('click', () => {
         plusSlides(1);
-    });
+    });*/
 });
